ebdd0c6757309309e4c5fbacb2cf4ba7d5fd40cd
Vadim V P <Everestxxx555@yandex.ru>
2022-12-22 01:30:59 +0300

    коммит Zadacha_29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 29
+// Напишите программу, которая задает массив из 8 
+// элементов и выводит их на экран.
+
+Console.Clear();
+// int DataVvod(string str)      // метод позволяющий задавать длинну массива
+// {
+//     Console.Write(str);
+//     int number = int.Parse(Console.ReadLine());
+//     return number;
+// }
+
+int[] ArrayData(int number)
+{
+   int[] array = new int[number];
+   int i = 0;
+  for(i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(0, 100);
+  } 
+  return array;
+}
+
+void PrintArray(int[] arrayPrint)
+{
+    for(int i = 0; i < arrayPrint.Length; i++)
+    {
+        Console.Write($"{arrayPrint[i]} ");
+    }
+}
+
+int numberSchet = 8;   // int numberSchet = DataVvod("Задайте длинну массива")
+int[] array = ArrayData(numberSchet);
+PrintArray(array);
